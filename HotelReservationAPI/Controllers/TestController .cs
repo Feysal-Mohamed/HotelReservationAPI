b34f9b9208ded8f91419418100cf49c508bf14b9
feysalow <fyslcs567@gmail.com>
2026-07-13 06:20:17 +0300

    git add . git commit -m "hsjdhf" git push

---
+++ HotelReservationAPI/Controllers/TestController .cs
@@ -0,0 +1,24 @@
+﻿using HotelReservationAPI.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+[ApiController]
+[Route("api/test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        try
+        {
+            using var con = new SqlConnection(AppConfig.ConnectionString);
+            con.Open();
+
+            return Ok("Database connected successfully");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.ToString());
+        }
+    }
+}
